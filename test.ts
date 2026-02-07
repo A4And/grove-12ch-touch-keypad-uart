1b26ef66ffa56a8d51f72433b28aa43837a664f4
A4And <bernot.and@gmail.com>
2026-02-07 19:12:33 +0100

    Update README.md, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+groveKeypad12ch.config(SerialPin.P0, SerialPin.P1)
+
+basic.forever(function () {
+    const k = groveKeypad12ch.lireClavier()
+    if (k >= 0) {
+        serial.writeLine("" + k)
+        basic.pause(200)
+        groveKeypad12ch.clear()
+    }
+    basic.pause(20)
+})
